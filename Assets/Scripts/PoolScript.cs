add68fd775dde39eb3553ba37a5b0a9b98e12e66
KamilBory <73657592+KamilBory@users.noreply.github.com>
2021-11-04 12:11:11 +0100

    InitialChanges

---
+++ Assets/Scripts/PoolScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolScript : MonoBehaviour
+{
+    public Vector3 moveVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveVec = new Vector3(-50.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += moveVec * Time.deltaTime;
+    }
+}
