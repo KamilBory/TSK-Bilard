ec09819cad914631380a75b522dec82b84319dc0
KamilBory <73657592+KamilBory@users.noreply.github.com>
2022-01-07 21:50:51 +0100

    Coraz bliżej

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class BallScript : MonoBehaviour
 {
+    private bool isGrounded;
+    private Vector3 gravity = new Vector3(0.0f, 0.0f, 0.0f);
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,31 @@ public class BallScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isGrounded)
+        {
+            gravity = new Vector3(0.0f, -15.0f, 0.0f);
+        }
+        else
+        {
+            gravity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        transform.position += gravity * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Table")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Table")
+        {
+            isGrounded = false;
+        }
     }
 }
