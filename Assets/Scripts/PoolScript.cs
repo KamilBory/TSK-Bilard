527e272495f26a9070f7b08b4b772f4f3c2a72e8
KamilBory <73657592+KamilBory@users.noreply.github.com>
2022-01-07 19:52:50 +0100

    Trochę nowości

--- Assets/Scripts/PoolScript.cs
+++ Assets/Scripts/PoolScript.cs
@@ -9,11 +9,13 @@ public class PoolScript : MonoBehaviour
     public Vector3 moveVec;
     public Slider slider;
     public Slider heightSlider;
+    public Slider powerSlider;
 
     public GameObject whiteBall;
 
     private float checker = 0.0f;
     private float heightChecker = 0.0f;
+    private Vector3 enrtyPosition = new Vector3(63.12f, 4.85f, -0.2f);
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,11 @@ public class PoolScript : MonoBehaviour
         heightSlider.onValueChanged.AddListener(delegate
         {
             ChangeHeight();
+        });
+
+        powerSlider.onValueChanged.AddListener(delegate
+        {
+            ChangeDistance();
         });
     }
 
@@ -46,10 +53,66 @@ public class PoolScript : MonoBehaviour
         float tmp = slider.value - checker;
         transform.RotateAround(whiteBall.transform.position, new Vector3 (0, 1, 0), tmp);
         checker = slider.value;
+
+        if(slider.value < 90.0f)
+        {
+            enrtyPosition = new Vector3(transform.position.x + powerSlider.value, transform.position.y, transform.position.z - powerSlider.value);
+        }
+        else if (slider.value >= 90.0f && slider.value < 180.0f)
+        {
+            enrtyPosition = new Vector3(transform.position.x - powerSlider.value, transform.position.y, transform.position.z - powerSlider.value);
+        }
+        else if (slider.value >= 180.0f && slider.value < 270.0f)
+        {
+            enrtyPosition = new Vector3(transform.position.x - powerSlider.value, transform.position.y, transform.position.z + powerSlider.value);
+        }
+        else if (slider.value >= 270.0f)
+        {
+            enrtyPosition = new Vector3(transform.position.x + powerSlider.value, transform.position.y, transform.position.z + powerSlider.value);
+        }
+
     }
 
     private void ChangeHeight()
     {
         transform.position = new Vector3(transform.position.x, heightSlider.value, transform.position.z);
+    }
+
+    private void ChangeDistance()
+    {
+        float degree = slider.value;
+        float x = 0.0f;
+        float z = 0.0f;
+
+        if(degree < 90.0f)
+        {
+            // dodaj x, odejmij z
+            float tmp = degree / 90.0f;
+            x = enrtyPosition.x + (1.0f - tmp) * powerSlider.value;
+            z = enrtyPosition.z - tmp * powerSlider.value;
+        }
+        else if(degree >= 90.0f && degree < 180.0f)
+        {
+            // odejmij x, odejmij z
+            float tmp = (degree - 90.0f) / 90.0f;
+            x = enrtyPosition.x - tmp * powerSlider.value;
+            z = enrtyPosition.z - (1.0f - tmp) * powerSlider.value;
+        }
+        else if(degree >= 180.0f && degree < 270.0f)
+        {
+            // odejmij x, dodaj z
+            float tmp = (degree - 180.0f) / 90.0f;
+            x = enrtyPosition.x - (1.0f - tmp) * powerSlider.value;
+            z = enrtyPosition.z + tmp * powerSlider.value;
+        }
+        else if(degree >= 270.0f)
+        {
+            // dodaj x, dodaj z
+            float tmp = (degree - 270.0f) / 90.0f;
+            x = enrtyPosition.x + tmp * powerSlider.value;
+            z = enrtyPosition.z + (1.0f - tmp) * powerSlider.value;
+        }
+
+        transform.position = new Vector3(x, transform.position.y, z);
     }
 }
