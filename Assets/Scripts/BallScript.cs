47e5c6dc71a62696a946519c17ca53338ae08774
Mateusz Dela <mateusz.dela.md@gmail.com>
2022-01-09 23:58:59 +0100

    cos

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -37,7 +37,11 @@ public class BallScript : MonoBehaviour
 
         //transform.position += gravity * Time.deltaTime;
 
-        //BallsCollision();
+        rigidbody.velocity = rigidbody.velocity * 0.984f;
+        if(rigidbody.velocity.magnitude < 0.1f) {
+            rigidbody.velocity = new Vector3(0, 0, 0);
+            rigidbody.angularVelocity = new Vector3(0, 0, 0);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -80,30 +84,6 @@ public class BallScript : MonoBehaviour
         if (collision.gameObject.name == "Table")
         {
             isGrounded = false;
-        }
-    }
-
-    private void BallsCollision(Collision collision) {
-        if (collision.gameObject.tag == "Ball") 
-        {
-            Vector3 normal = gameObject.transform.position - collision.transform.position;
-
-            Vector3 unitNormal = normal * (1 / normal.magnitude);
-
-            Vector3 unitTangent = new Vector3(-normal.z, normal.y, normal.x);
-
-            float velNormal1 = Vector3.Dot(unitNormal, rigidbody.velocity);
-            float velTangent1 = Vector3.Dot(unitTangent, rigidbody.velocity);
-            float velNormal2 = Vector3.Dot(unitNormal, collision.rigidbody.velocity);
-            float velTangent2 = Vector3.Dot(unitTangent, collision.rigidbody.velocity);
-
-            Vector3 velNormalTag1 = unitNormal * velNormal1;
-            Vector3 velTangentTag1 = unitTangent * velTangent1;
-            Vector3 velNormalTag2 = unitNormal * velNormal2;
-            Vector3 velTangentTag2 = unitTangent * velTangent2;
-
-            rigidbody.AddForce(velNormalTag1 + velTangentTag1);
-            collision.rigidbody.AddForce(velNormalTag2 + velTangentTag2);
         }
     }
 }
